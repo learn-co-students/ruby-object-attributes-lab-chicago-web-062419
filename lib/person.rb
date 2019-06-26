c629d37dbd1b4c56428211d2fa5063e1c0604aca
github username <github email address>
2019-06-26 14:50:01 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+    # Writes the argument "name" to in instance variable @name.
+    def name=(name)
+        @name = name
+    end
+
+    # Sets up the instance variable @name
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+end
