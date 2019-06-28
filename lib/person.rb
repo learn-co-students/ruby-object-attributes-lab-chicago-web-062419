f3ca8ddf3e33dc6c52a8fbf3a3c2aaf1cde8b50d
Erica Hawkins <ehawk901@gmail.com>
2019-06-28 13:12:03 -0500

    Done

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+    
+    def name= person
+        @name= person
+    end 
+
+    def name
+        @name  
+    end 
+ 
+    def job= persons_job
+        @job= persons_job 
+    end 
+
+    def job
+        @job
+    end 
+
+end 
+
